8536e69427a0b19496f7e0990804b25f14c71611
andy chung <chung.andy@gmail.com>
2012-06-08 23:16:15 -0400

    changed how we're dealing with errors.

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # jQuery Expected
 
-jQuery Expected is a simple way to turn this:
+jQuery Expected is inspired by .NET's [Enumerable.Single](http://msdn.microsoft.com/en-us/library/system.linq.enumerable.single.aspx). It takes in an expected value and returns the collection if the number of items in the selector array matches the expected value. It throws an Error if the length of the array doesn't match.
 
-    if ($('selector').length !== 1) {
-        // handle error
-    } else {
-        $('selector') // do something
+* When the expected value doesn't match, an Error of type ExpectedValueError is thrown. ExpectedValueError is included in the global namespace.
+
+example:
+
+    try {
+        $('selector').expected(1).html('hello world');
+    } catch (ex) {
+        if (ex instanceof ExpectedValueError) {
+            alert('Selector did not have expected length of 1');
+        } else {
+            console.log(ex);
+        }
     }
-
-into this:
-
-    $('selector').expected(1, function () {
-        // handle error
-    }) // do something
--- jquery.expected.js
+++ jquery.expected.js
@@ -2,9 +2,33 @@
 // scripts and/or other plugins which may not be closed properly.
 ;(function ($, window, undefined) {
 	$.fn.expected = function (expectedVal) {
-		if (this.length !== expectedVal) {
-			throw 'Array returned by selector did not have expected length of ' + expectedVal.toString();
+		// if expectedVal is a string, try parsing number
+		if (typeof (expectedVal) === 'string') {
+			expectedVal = parseInt(expectedVal);
 		}
+		if (!isNumber(expectedVal)) {
+			throw new ExpectedValueError('Expected value is not a valid number');
+		}
+		
+		// check if length matches expected value
+		if (this.length !== expectedVal) {
+			throw new ExpectedValueError('Array returned by selector did not have expected length of ' + expectedVal.toString());
+		}
+		
+		// return original selector array
 		return this;
 	}
+	
+	// internal function used to check if n is a number
+	function isNumber(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+	
 }(jQuery, window));
+
+// custom error object used by plugin
+function ExpectedValueError(msg) {
+	this.name = 'ExpectedValueError';
+	this.message = msg || '';
+}
+ExpectedValueError.prototype = new Error();
