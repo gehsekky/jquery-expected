ffeec2a85d7233b5cea5cbbd544caf928eab35ad
andy chung <chung.andy@gmail.com>
2012-06-08 20:23:09 -0400

    finished initial version of plugin and test script.

--- jquery.expected.js
+++ jquery.expected.js
@@ -1,7 +1,7 @@
 // the semi-colon before function invocation is a safety net against concatenated 
 // scripts and/or other plugins which may not be closed properly.
-;(function ($, window, undefined) {	
-	$.fn.expected = function (expectedVal, runtimeOptions) {
+;(function ($, window, undefined) {
+	$.fn.expected = function (expectedVal) {
 		if (this.length !== expectedVal) {
 			throw 'Array returned by selector did not have expected length of ' + expectedVal.toString();
 		}
